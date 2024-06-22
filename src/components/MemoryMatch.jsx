1564bdf5181e590df768a02f0c80cb5bbe069bdf
Abi Garner <abigail.gee@hotmail.com>
2024-06-22 02:31:13 -0400

    adding notes

--- src/components/MemoryMatch.jsx
+++ src/components/MemoryMatch.jsx
@@ -3,7 +3,11 @@ import "../components/MemoryMatch.css";
 
 const symbols = [1,2,3,4,5];
 const allSym = shuffle([...symbols,...symbols]);
+///creating symbols to represent the cards that will be used in the game//
+///using the shuffle method and spreading the symbols so that they will be shuffled every time 
+// the game restarts.//
 
+//// creating component for the actual game .///
 export default function MemoryMatch() {
   return (
     <div>MemoryMatch</div>
