05dca385fc0f0bf81fc64fa4b11972e161c9ec1e
Abi Garner <abigail.gee@hotmail.com>
2024-06-22 02:28:36 -0400

    more styling and adjustments

--- src/components/MemoryMatch.jsx
+++ src/components/MemoryMatch.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import "../components/MemoryMatch.css";
-import '@fontsource-variable/dosis';
 
+const symbols = [1,2,3,4,5];
+const allSym = shuffle([...symbols,...symbols]);
 
 export default function MemoryMatch() {
   return (
