e179091cf3b5b6e0cf9056c8dda1451ff6636a87
Abi Garner <abigail.gee@hotmail.com>
2024-06-22 02:03:27 -0400

    finally imported font

---
+++ src/components/MemoryMatch.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../components/MemoryMatch.css";
+import '@fontsource-variable/dosis';
+
+
+export default function MemoryMatch() {
+  return (
+    <div>MemoryMatch</div>
+  )
+}
