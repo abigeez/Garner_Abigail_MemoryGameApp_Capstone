8e06423ffbf87122a5e96283a14cf315918da927
Abi Garner <abigail.gee@hotmail.com>
2024-06-22 01:47:18 -0400

    adding new component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Sound Memory Match</h1>
+      <h1>Sound Memory Match Game</h1>
       <button>Start</button>
 
     </div>
