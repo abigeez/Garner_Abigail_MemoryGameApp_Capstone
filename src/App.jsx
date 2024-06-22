46e1fb50528f9058585fb3f9fae241282210147d
Abi Garner <abigail.gee@hotmail.com>
2024-06-22 01:09:52 -0400

    modifying boilerplate and editing css

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-
   return (
-    <>
+    <div className="App">
+      <h1>Sound Memory Match Game</h1>
+      <button>Start</button>
+
+    </div>
       
-    </>
+    
   )
 }
 
