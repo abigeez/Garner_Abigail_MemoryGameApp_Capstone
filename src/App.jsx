c700d0cc94be042ed183a87a17babbf0e434fa37
Abi Garner <abigail.gee@hotmail.com>
2024-06-23 02:42:30 -0400

    using the sort and random method helps to shuffle the numbers by using sort to return a number greater than 0 which means that the cards need to be shuffled and randomized

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,9 @@ function App() {
   //creating a function to duplicate the cards so that they can be matched together in the game
   //and then using the sort method to shuffle them up and then be assigned an id to be able to match them
 
-  const shflCards = () => {
-    const cardsShuffled = [...imgCards]
+  const cardShuffle = () => {
+    const cardsShuffled = [...imgCards,...imgCards]
+    .sort(()=> Math.random() - 0.5 )
   }
 
   return (
