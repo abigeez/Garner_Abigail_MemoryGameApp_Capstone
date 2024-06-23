7dfc397d7c6b85c8ebe28dd8ffada9dc371c8b6a
Abi Garner <abigail.gee@hotmail.com>
2024-06-23 01:38:06 -0400

    starting new function

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,22 @@ const imgCards = [
   { "src": "/images/dino4Card.png" },
   { "src": "/images/dino5Card.png" },
   { "src": "/images/dino6Card.png" }
-]
+];
 
 ///creating an  array  of obejcts for the cards to remain constant because 
 // the cards  dont need to be recreated every time the component is reevaluated.
 
 
 function App() {
+
+  //creating a function to duplicate the cards so that they can be matched together in the game
+  //and then using the sort method to shuffle them up and then be assigned an id to be able to match them
+
+  const shflCards = 
+
   return (
     <div className="App">
-      <h1>Dinosaur Memory Match Game</h1>
+      <h1>Dinosaur Match Memory Game</h1>
       <button>START</button>
 
     </div>
