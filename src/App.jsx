9e90ce100b2d9f3a8d0b1df8bfdcc14634a9f9f0
Abi Garner <abigail.gee@hotmail.com>
2024-06-23 01:05:03 -0400

    inserting source link for images

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 
-const imgCards = []
+const imgCards = [
+  { "src": "/images/dino1Card.png" },
+  { "src": "/images/dino2Card.png" },
+  { "src": "/images/dino3Card.png" },
+  { "src": "/images/dino4Card.png" },
+  { "src": "/images/dino5Card.png" },
+  { "src": "/images/dino6Card.png" }
+]
 
 ///creating an  array  of obejcts for the cards to remain constant because 
 // the cards  dont need to be recreated every time the component is reevaluated.
@@ -15,8 +22,8 @@ function App() {
       <button>START</button>
 
     </div>
-      
-    
+
+
   )
 }
 
