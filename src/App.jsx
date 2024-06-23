ae3a05a74088e622e2ee2df8bf321c462c5e8c84
Abi Garner <abigail.gee@hotmail.com>
2024-06-23 03:11:05 -0400

    adding more comments to the establish the steps ive taken

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 
 import './App.css'
 
+
+
+///creating an  array  of obejcts for the cards to remain constant because 
+// the cards  dont need to be recreated every time the component is reevaluated.
+
+
 const imgCards = [
   { "src": "/images/dino1Card.png" },
   { "src": "/images/dino2Card.png" },
@@ -11,19 +17,23 @@ const imgCards = [
   { "src": "/images/dino6Card.png" }
 ];
 
-///creating an  array  of obejcts for the cards to remain constant because 
-// the cards  dont need to be recreated every time the component is reevaluated.
-
 
 function App() {
 
-  //creating a function to duplicate the cards so that they can be matched together in the game
-  //and then using the sort method to shuffle them up and then be assigned an id to be able to match them
+
+  //creating a state to store the cards in for each individual game that is played to finish
+
+const [gameCards,setgameCards] = useState([])
+
+
+
+  //creating a function to duplicate the cards so that they can be matched and then using the sort method 
+  //to shuffle them up based on what it returns and then be assigned an id to be able to match them
 
   const cardShuffle = () => {
-    const cardsShuffled = [...imgCards,...imgCards]
-    .sort(()=> Math.random() - 0.5 )
-    .map((cardId) => ({...cardId}))
+    const cardsShuffled = [...imgCards, ...imgCards]
+      .sort(() => Math.random() - 0.5)
+      .map((gameCard) => ({ ...gameCard, id: Math.random() }))
   }
 
   return (
