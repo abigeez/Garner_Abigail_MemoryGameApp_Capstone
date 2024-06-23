fdb779501c9d39938cd9895b2b11667455197c19
Abi Garner <abigail.gee@hotmail.com>
2024-06-23 03:29:20 -0400

    Adding a new state that is rendered after every click event of the start button to begin a new game

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   //creating a state to store the cards in for each individual game that is played to finish
 const [gameCards,setGameCards] = useState([])
-
+const [attempts, setAttempts] = useState(0)
   //creating a function to duplicate the cards so that they can be matched and then using the sort method 
   //to shuffle them up based on what it returns and then be assigned an id to be able to match them
   const cardShuffle = () => {
