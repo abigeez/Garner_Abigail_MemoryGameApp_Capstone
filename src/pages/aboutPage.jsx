e7ee89e4ed235b5fa9adec0d8f0bf327834c4781
Abi Garner <abigail.gee@hotmail.com>
2024-06-26 05:19:35 -0400

    almost done with pages

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import prof_pic from '../img/profilePic.jpg'
 import '../pages/AboutPage.css'
+import welc_dino from '../img/leggoDino.png'
 function AboutPage() {
   return (
     <div className="itsMe">
@@ -12,7 +13,10 @@ function AboutPage() {
         <span className="aboutMe">Hi, my name is Abi and I'm the designer and engineer behind the Dinosaur Match Memory Game. 
           I am a Software Engineer who recently graduated from Per Scholas Software Engineering Certification Program. 
           This project was my Capstone project where I had to implement a Full-stack MERN app showcasing all the skills that I've learned during the program. The inspiration behind this game is that Jurassic Park was one of my favorite films growing up! I hope you enjoy playing it as much as I enjoyed making it.   </span>
-      </div>
+          
+          
+      </div> 
+      
       </div>
   )
 }
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import welc_dino from '../img/leggoDino.png'
 function LandingPage() {
     return (
         <div>
             <Navbar></Navbar>
-            <div>Home</div>
+            <div className="homePage">Welcome to The Dinosaur Memory Game!</div>
+            <a href="/playgame"><img className="dinoLink"src={welc_dino} /> </a>
         </div>
     )
 }
