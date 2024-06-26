7bff9c2e54ab12343bd9fab1b71434d4574f18f8
Abi Garner <abigail.gee@hotmail.com>
2024-06-26 00:30:18 -0400

    added more css to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,17 @@ import "../components/Navbar.css"
 export default function Navbar() {
 
   return <nav className="navBar">
-<a href ="">Dinosaur Memory Match Game</a>
-
+<a href ="/" className="siteName">Dinosaur Memory Match Game</a>
+<ul>
+  <li> 
+    <a href ="/">Home</a> </li>
+   <li> <a href ="/instructions">About</a></li>
+    <li><a href ="/playgame">Game</a></li>
+   <li> <a href ="/reviews">Reviews</a>
+  </li>
+</ul>
   </nav>
     
   
 }
+
